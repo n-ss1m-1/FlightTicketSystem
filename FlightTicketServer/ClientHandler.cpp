1ae74e4e5f7d4b23c0185804e99c7765d97d97a5
c3 <3233851773@qq.com>
2025-12-22 14:06:05 +0800

    添加功能:查询航班 创建订单 查询订单 取消订单

--- FlightTicketServer/ClientHandler.cpp
+++ FlightTicketServer/ClientHandler.cpp
@@ -46,7 +46,8 @@ void ClientHandler::handleJson(const QJsonObject &obj)
     // 获取数据库单例
     DBManager &db = DBManager::instance();
 
-    // ==================== 1. 登录 ====================
+    QString errMsg;
+    //登录
     if (type == Protocol::TYPE_LOGIN) {
         const QString username = data.value("username").toString();
         const QString password = data.value("password").toString();
@@ -54,10 +55,10 @@ void ClientHandler::handleJson(const QJsonObject &obj)
         qInfo() << "Login request:" << username;
 
         Common::UserInfo user;
-        QString errorMsg;
 
-        // 1. 调用队友写的查询函数
-        DBResult res = db.getUserByUsername(username, user, &errorMsg);
+
+        // 1. 调用查询函数
+        DBResult res = db.getUserByUsername(username, user, &errMsg);
 
         // 2. 校验结果 (DBResult::Success 且 密码匹配)
         // 注意：这里假设数据库里存的是明文密码，实际开发通常要加密，但作业可以直接比对
@@ -70,7 +71,7 @@ void ClientHandler::handleJson(const QJsonObject &obj)
         return;
     }
 
-    // ==================== 2. 注册 ====================
+    //注册
     else if (type == Protocol::TYPE_REGISTER) {
         const QString username = data.value("username").toString();
         const QString password = data.value("password").toString();
@@ -87,7 +88,7 @@ void ClientHandler::handleJson(const QJsonObject &obj)
             return;
         }
 
-        QString errMsg;
+
         DBResult ret = db.addUser(username,password,phone,realName,idCard,&errMsg);
 
         if (ret == DBResult::Success) {
@@ -99,7 +100,7 @@ void ClientHandler::handleJson(const QJsonObject &obj)
         return;
     }
 
-    // ==================== 3. 修改密码 ====================
+    //修改密码
     else if (type == Protocol::TYPE_CHANGE_PWD) {
         const QString username = data.value("username").toString();
         const QString oldPwd = data.value("oldPassword").toString();
@@ -117,7 +118,7 @@ void ClientHandler::handleJson(const QJsonObject &obj)
         }
 
         // 2. 更新新密码
-        QString errMsg;
+
         res=db.updatePasswdByUsername(username,newPwd,&errMsg);
         if (res == DBResult::Success) {
             sendJson(Protocol::makeOkResponse(Protocol::TYPE_CHANGE_PWD_RESP, QJsonObject(), "密码修改成功"));
@@ -126,8 +127,139 @@ void ClientHandler::handleJson(const QJsonObject &obj)
         }
         return;
     }
+    //根据出发地+目的地+日期查询航班
+    else if(type == Protocol::TYPE_FLIGHT_SEARCH)
+    {
+        const QString fromCity=data.value("fromCity").toString();
+        const QString toCity=data.value("toCity").toString();
+        const QDate date=QDate::fromString(data.value("date").toString(), Qt::ISODate);
 
-    // ... 其他逻辑 (如 UPDATE_INFO) 可以依样画葫芦 ...
+        qInfo() << "search flights request: from" << fromCity << "to" << toCity << "date" << date.toString(Qt::ISODate);
+
+        if (fromCity.isEmpty()) {
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR, "出发城市不能为空"));
+            return;
+        }
+        if (toCity.isEmpty()) {
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR, "到达城市不能为空"));
+            return;
+        }
+        if (!date.isValid()) {
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR, "查询日期格式无效"));
+            return;
+        }
+
+        //查询航班信息
+        QList<Common::FlightInfo> flights;
+
+        DBResult res = db.searchFlights(fromCity,toCity,date,flights,&errMsg);
+        if(res == DBResult::Success)
+        {
+            QJsonArray flightsArr = Common::flightsToJsonArray(flights);
+            QJsonObject respData;
+            respData.insert("flights",flightsArr);
+            respData.insert("count",flights.size());
+            sendJson(Protocol::makeOkResponse(Protocol::TYPE_FLIGHT_SEARCH_RESP,respData,QString("航班查询成功,查询到%1条航班").arg(flights.size())));
+        }
+        else
+        {
+            qCritical()<<"flight search error:"<<errMsg;
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR,"航班查询失败"+errMsg));
+        }
+        return;
+    }
+    //创建订单
+    else if(type == Protocol::TYPE_ORDER_CREATE)
+    {
+        //需要客户端传入：user_id,flight_id,passenger_name,passenger_id_card (可以使用一个user给多个不同的passenger创建订单？)
+        Common::OrderInfo order;
+        order.userId=data.value("userId").toVariant().toLongLong();
+        order.flightId=data.value("flightId").toVariant().toLongLong();
+        order.passengerName=data.value("passengerName").toString();
+        order.passengerIdCard=data.value("passengerIdCard").toString();
+        if (order.userId<=0) {
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR, "用户id不能<=0"));
+            return;
+        }
+        if (order.flightId<=0) {
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR, "航班id不能<=0"));
+            return;
+        }
+        if (order.passengerName.isEmpty()) {
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR, "乘客姓名不能为空"));
+            return;
+        }
+        if (order.passengerIdCard.isEmpty()) {
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR, "乘客IdCard不能为空"));
+            return;
+        }
+
+        DBResult res=db.createOrder(order,&errMsg);
+        if(res == DBResult::Success)
+        {
+            QJsonObject orderObj = Common::orderToJson(order);
+            QJsonObject respData;
+            respData.insert("order",orderObj);              //包含order的所有信息
+            sendJson(Protocol::makeOkResponse(Protocol::TYPE_ORDER_CREATE_RESP,respData,QString("订单创建成功,订单号：%1").arg(order.id)));
+        }
+        else
+        {
+            qCritical()<<"order create error:"<<errMsg;
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR,"订单创建失败"+errMsg));
+        }
+        return;
+    }
+    //查询用户所有订单(根据userId)
+    else if(type == Protocol::TYPE_ORDER_LIST)
+    {
+        const qint64 userId=data.value("userId").toVariant().toLongLong();
+        if (userId<=0) {
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR, "用户id不能<=0"));
+            return;
+        }
+
+        QList<Common::OrderInfo> orders;
+        DBResult res=db.getOrdersByUserId(userId,orders,&errMsg);
+        if(res == DBResult::Success)
+        {
+            QJsonArray orderArr = Common::ordersToJsonArray(orders);
+            QJsonObject respData;
+            respData.insert("orders",orderArr);
+            sendJson(Protocol::makeOkResponse(Protocol::TYPE_ORDER_LIST_RESP,respData,QString("查询到%1条订单").arg(orders.size())));
+        }
+        else
+        {
+            qCritical()<<"order search error:"<<errMsg;
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR,"订单查询失败"+errMsg));
+        }
+        return;
+    }
+    //取消订单(根据userId和orderId)
+    else if(type == Protocol::TYPE_ORDER_CANCEL)
+    {
+        const qint64 userId=data.value("userId").toVariant().toLongLong();
+        const qint64 orderId=data.value("orderId").toVariant().toLongLong();
+        if (userId<=0) {
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR, "用户id不能<=0"));
+            return;
+        }
+        if (orderId<=0) {
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR, "orderid不能<=0"));
+            return;
+        }
+
+        DBResult res=db.cancelOrder(userId,orderId,&errMsg);
+        if(res == DBResult::Success)
+        {
+            sendJson(Protocol::makeOkResponse(Protocol::TYPE_ORDER_CANCEL_RESP,QJsonObject(),QString("订单取消成功")));
+        }
+        else
+        {
+            qCritical()<<"order cancel error"<<errMsg;
+            sendJson(Protocol::makeFailResponse(Protocol::TYPE_ERROR,"订单取消失败"+errMsg));
+        }
+        return;
+    }
 
     // 未知请求
     sendJson(Protocol::makeFailResponse(
