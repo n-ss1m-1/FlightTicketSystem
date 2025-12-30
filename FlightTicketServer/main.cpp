5f98ccd4bc7c0ffd167bf79ef9e037675922020f
Aung <alhalh.aa69.aa@gmail.com>
2025-12-30 17:28:43 +0800

    修改

--- FlightTicketServer/main.cpp
+++ FlightTicketServer/main.cpp
@@ -1,7 +1,7 @@
 #include <QApplication>
 #include "ServerWindow.h"
 #include "DBManager.h"
-
+#include <QInputDialog>
 // 这里的密码改成你自己的！
 static QString DBpassword = "你的密码";
 
@@ -9,14 +9,23 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    // 初始化数据库连接
-    // 这里的 "root" 和 DBpassword 根据你的实际情况修改
-    if (!DBManager::instance().connect("127.0.0.1", 3306, "root", DBpassword, "flight_ticket")) {
-        // 如果连不上，打印个错误，但在界面出来前可能看不到
+    // 需求2：手动输入数据库密码
+    bool ok;
+    QString text = QInputDialog::getText(nullptr, "安全验证",
+                                         "请输入数据库密码:", QLineEdit::Password,
+                                         "", &ok);
+    // 如果点了取消，或者密码为空(看你是否允许空)，直接退出
+    if (!ok) {
+        return 0;
+    }
+
+    // 这里的 text 就是用户输入的密码
+    // 把下面连接数据库的 "root" 改成 text
+    if (!DBManager::instance().connect("127.0.0.1", 3306, "root", text, "flight_ticket")) {
+        // ... 报错代码 ...
     }
 
     ServerWindow w;
     w.show();
-
     return a.exec();
 }
